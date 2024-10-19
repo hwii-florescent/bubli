46be97a656a6b664bc9945b73949e17a5c809a24
hwii <116895905+hwii-florescent@users.noreply.github.com>
2024-10-19 19:01:18 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.VITE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_APP_FIREBASE_SENDER_ID,
+  appId: process.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
