2f73a3146d814949dc090d8fb75a2bb4a7c94326
Vinh Nguyen <ntvinhgv@gmail.com>
2024-10-20 09:19:12 -0400

    Prepare to deploy

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://127.0.0.1:8000'
-		: 'https://whispering-fortress-65647.herokuapp.com/api'
+		: 'https://bubli-fk0s.onrender.com'
